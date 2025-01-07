428702f368abbcfbeb017c1cc5e0f9fd15d91ce7
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-07 12:04:04 +0300

    1408. String Matching in an Array - Optimized Beats 100% in Runtime - 0ms

--- Leetcode/1408. String Matching in an Array/main.cpp
+++ Leetcode/1408. String Matching in an Array/main.cpp
@@ -1,16 +1,13 @@
-#include <iostream>
-#include <unordered_set>
-using namespace std;
-
 class Solution {
 public:
-    vector<string> stringMatching(vector<string>& words) {
+    vector<string> stringMatching(vector<string> &words) {
         vector<string> result;
-
-        for(int i =0 ;i< words.size(); i++) {
-            for(int j =0; j< words.size(); j++) {
-                if(words[j].length() > words[i].length() && words[j].find(words[i]) != string::npos && count(result.begin(), result.end(), words[i]) ==0 ) {
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+                if (words[j].length() > words[i].length() && words[j].find(words[i]) != string::npos && count(
+                        result.begin(), result.end(), words[i]) == 0) {
                     result.push_back(words[i]);
+                    break;
                 }
             }
         }
@@ -18,10 +15,3 @@ public:
     }
 };
 
-
-int main() {
-    Solution solution;
-    vector<string> words = {"mass","as", "as","hero", "hero","superhero"};
-    solution.stringMatching(words);
-    return 0;
-}
