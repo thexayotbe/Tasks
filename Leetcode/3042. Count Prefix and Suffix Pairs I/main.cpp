b4466527d5042af0a05f323203650c504a133dae
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-08 12:02:55 +0300

    3042. Count Prefix and Suffix Pairs I - Solved. Beats 100% in Runtime - 0ms

--- Leetcode/3042. Count Prefix and Suffix Pairs I/main.cpp
+++ Leetcode/3042. Count Prefix and Suffix Pairs I/main.cpp
@@ -4,12 +4,10 @@ public:
         int counts = 0;
         for (int i = 0; i < words.size(); i++) {
             for (int j = i; j < words.size(); j++) {
-                if (j == i)
-                    continue;
-                if (words[j].starts_with(words[i]) &&
-                    words[j].ends_with(words[i])) {
+                if (j == i) continue;
+                if (words[j].starts_with(words[i]) && words[j].ends_with(words[i])) {
                     counts += 1;
-                    }
+                }
             }
         }
         return counts;
