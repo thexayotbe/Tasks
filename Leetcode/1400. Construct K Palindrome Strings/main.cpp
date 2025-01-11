0796b92f66322f7a77b12aff8781b38e0d22fd3c
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-11 13:43:00 +0300

    Optimized code. Beats 100% in Runtime 0ms.

--- Leetcode/1400. Construct K Palindrome Strings/main.cpp
+++ Leetcode/1400. Construct K Palindrome Strings/main.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     bool canConstruct(string s, int k) {
         int n = s.length();
-        if(n < k) return false;
+        if (n < k)
+            return false;
         int count[26] = {0};
-        for(char c: s) count[c-'a']++;
+        for (char c : s)
+            count[c - 'a']++;
         int countOdd = 0;
-        for(int i = 0; i < 26; i++) {
-            if(count[i] % 2 !=0) countOdd++;
+        for (int i = 0; i < 26; i++) {
+            if (count[i] % 2 != 0)
+                countOdd++;
         }
-        return countOdd <=k;
+        return countOdd <= k;
     }
 };
