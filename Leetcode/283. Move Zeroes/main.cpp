89a52bd47c263f620e649d78a532f5b9585a615b
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-11 14:18:12 +0300

    283. Move Zeroes - Optimized Beats 100% in Runtime 0ms

--- Leetcode/283. Move Zeroes/main.cpp
+++ Leetcode/283. Move Zeroes/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-using namespace std;
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
@@ -10,20 +8,9 @@ public:
                 slow++;
             }
         }
-
         while(slow < nums.size()) {
             nums[slow] = 0;
             slow++;
         }
-        for(int nums : nums) {
-            cout << nums << ' ';
-        }
     }
 };
-int main() {
-    vector<int>  nums = {0,1,0,3,12};
-    Solution s;
-    s.moveZeroes(nums);
-
-    return 0;
-}
