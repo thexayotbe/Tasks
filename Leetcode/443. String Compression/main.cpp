e928663ca55a60fa47deacd9547fedd83db3b97c
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-11 15:45:00 +0300

    443. String Compression -  Optimizes Beats 100% in runtime 0ms

--- Leetcode/443. String Compression/main.cpp
+++ Leetcode/443. String Compression/main.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int compress(vector<char> &chars) {
+    int compress(vector<char>& chars) {
         int n = chars.size();
-        if (n == 0) return 0;
+        if (n == 0)
+            return 0;
 
         int write = 0;
         int read = 0;
 
-        while(read < n) {
+        while (read < n) {
             char current = chars[read];
             int count = 0;
-
-            while(read < n && current == chars[read]) {
+            while (read < n && current == chars[read]) {
                 read++;
                 count++;
             }
             chars[write++] = current;
 
-            if(count > 1) {
-                for(char digit : to_string(count)) {
+            if (count > 1) {
+                for (char digit : to_string(count)) {
                     chars[write++] = digit;
                 }
             }
