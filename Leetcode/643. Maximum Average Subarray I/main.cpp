23c2b4dd5a19c07c8e26b02dd3d512abc8f5a709
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-13 15:25:18 +0300

    643. Maximum Average Subarray I - Solved - Optimized - Beats 100% in Runtime 0ms

--- Leetcode/643. Maximum Average Subarray I/main.cpp
+++ Leetcode/643. Maximum Average Subarray I/main.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        if(nums.size() < k) return 0;
+        if (nums.size() < k)
+            return 0;
         double windowSum = 0;
 
         for (int i = 0; i < k; i++) {
@@ -11,10 +12,10 @@ public:
         double maxSum = windowSum;
 
         for (int i = k; i < nums.size(); i++) {
-
-            windowSum += nums[i] - nums[i - k];
+            windowSum += nums[i];
+            windowSum -= nums[i - k];
             maxSum = max(maxSum, windowSum);
         }
-        return maxSum/k;
+        return maxSum / k;
     }
 };
