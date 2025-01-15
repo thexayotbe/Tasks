0aa17077d55340f68bd2ad31933ce330a83b272c
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-15 22:14:51 +0300

    1732. Find the Highest Altitude - Optimizes Beats 100% in Runtime - 0ms.

--- Leetcode/1732. Find the Highest Altitude/main.cpp
+++ Leetcode/1732. Find the Highest Altitude/main.cpp
@@ -4,7 +4,7 @@ public:
         int n = gain.size();
         vector<int> prefixSum(n+1, 0);
         int maxGain = 0;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             int sum = prefixSum[i-1] + gain[i-1];
             maxGain = max(maxGain, sum);
             prefixSum[i] = sum;
