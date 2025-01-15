353a652146b066516f2f001021b655df6c019d09
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-15 16:20:52 +0300

    2429. Minimize XOR - Solved

---
+++ Leetcode/2429. Minimize XOR/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    // Function to convert decimal to binary and return as a string
+    int countSetBits(int num) {
+        int count = 0;
+        while(num) {
+            count += (num & 1);
+            num >>= 1;
+        }
+        return count;
+    }
+    int addSetBits(int num, int bitsToAdd) {
+        int bitPos = 0;
+        while(bitsToAdd>0) {
+            while ((num >> bitPos) & 1) ++bitPos;
+            num |= (1 << bitPos);
+            --bitsToAdd;
+        }
+        return num;
+    }
+    int removeSetBits(int num, int bitsToRemove) {
+        while (bitsToRemove > 0) {
+            num &= (num - 1);
+            --bitsToRemove;
+        }
+        return num;
+    }
+    int minimizeXor(int num1, int num2) {
+        int setBitsNum1 = countSetBits(num1);
+        int setBitsNum2 = countSetBits(num2);
+        if (setBitsNum1 == setBitsNum2) return num1;
+        if (setBitsNum1 < setBitsNum2) return addSetBits(num1, setBitsNum2 - setBitsNum1);
+        return removeSetBits(num1, setBitsNum1 - setBitsNum2);
+
+    }
+};
+int main() {
+    Solution s;
+    int  num1 = 3, num2 = 5;
+    std::cout << s.minimizeXor(num1, num2) << std::endl;
+    return 0;
+}
