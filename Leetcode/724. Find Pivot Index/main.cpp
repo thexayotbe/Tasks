518068503c7086b70f43c2c66cfe652071054473
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-16 13:28:57 +0300

    724. Find Pivot Index - Optimized Beats 100% in Runtime - 0ms

--- Leetcode/724. Find Pivot Index/main.cpp
+++ Leetcode/724. Find Pivot Index/main.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int pivotIndex(vector<int> &nums) {
-       int size = nums.size();
+        int size = nums.size();
         int total = accumulate(nums.begin(), nums.end(), 0);
         int leftTotal = 0;
         for (int i = 0; i < size; i++) {
