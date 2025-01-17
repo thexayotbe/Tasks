fffeca07e173c3a976116ff3c49195b65d518b2f
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-17 15:18:00 +0300

    2683. Neighboring Bitwise XOR - Optimized Beats 100% in Runtime - 0ms

--- Leetcode/2683. Neighboring Bitwise XOR/main.cpp
+++ Leetcode/2683. Neighboring Bitwise XOR/main.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        int n = derived.size();
-        for(int first : {0,1}) {
-            vector<int> original(n,0);
-            original[0] = first;
-            for(int i = 0 ;i < n;++i) {
-                original[i+1] = derived[i] ^ original[i];
-            }
-            if(original[n-1] ^ original[0] == derived[n-1]) {
-                return true;
-            }
-        }
-        return false;
+        return accumulate(derived.begin(), derived.end(), 0, bit_xor<>())==0;
     }
 };
-
