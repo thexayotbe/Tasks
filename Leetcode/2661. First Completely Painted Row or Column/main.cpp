1d34e828c3c166c004f1e1ff0ea69986ca16a59e
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-20 22:25:51 +0300

    2661. First Completely Painted Row or Column - Started

---
+++ Leetcode/2661. First Completely Painted Row or Column/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int > arr = {1,3,4,2};
+    vector<vector<int>> mat = {{1,4},{2,3}};
+    std::cout <<solution.firstCompleteIndex(arr, num)  << std::endl;
+    return 0;
+}
