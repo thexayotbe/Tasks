6b53b1aba68223d52237990e9cea949e43b977ed
Xayotbek Mamajonov <116886834+thexayotbe@users.noreply.github.com>
2025-01-20 22:53:08 +0300

    2661. First Completely Painted Row or Column - Solved

--- Leetcode/2661. First Completely Painted Row or Column/main.cpp
+++ Leetcode/2661. First Completely Painted Row or Column/main.cpp
@@ -4,20 +4,20 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
       unordered_map<int, pair<int,int>> hashmap;
-        int n = arr.size();
 
-        int col = mat.size();
-        int row = mat[0].size();
-        for (int i = 0; i < col; i++) {
-            for (int j = 0; j < row; j++) {
+        int m = mat.size();
+        int n = mat[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 hashmap[mat[i][j]] = {i,j};
             }
         }
 
-        vector<int> colCount(col, 0);
-        vector<int> rowCount(row, 0);
+        vector<int> colCount(n, 0);
+        vector<int> rowCount(m, 0);
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < arr.size(); i++) {
             int value = arr[i];
             auto position = hashmap[value];
             int row = position.first;
@@ -26,7 +26,7 @@ public:
             colCount[col]++;
             rowCount[row]++;
 
-            if(colCount[col] == row || rowCount[row] == row) {
+            if(colCount[col] == m || rowCount[row] == n) {
                 return i;
             }
         }
